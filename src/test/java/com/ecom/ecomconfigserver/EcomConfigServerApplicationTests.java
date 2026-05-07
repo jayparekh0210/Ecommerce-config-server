8609d4a80dc912dcefb011d97fe139ed399a59d2
Jay <jayparekhin02@gmail.com>
2026-05-07 15:27:35 +0530

    Added spring cloud bus

---
+++ src/test/java/com/ecom/ecomconfigserver/EcomConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
